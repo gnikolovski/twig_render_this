dd283bbb141c083144882ff637ef6495d5e4bf2b
goran_pds <goran_pds@3451979.no-reply.drupal.org>
2019-11-09 21:36:42 +0100

    Issue #3080806 by gnikolovski: Expand tests

--- tests/src/Functional/TwigRenderThisTest.php
+++ tests/src/Functional/TwigRenderThisTest.php
@@ -15,9 +15,12 @@ class TwigRenderThisTest extends BrowserTestBase {
    * {@inheritdoc}
    */
   public static $modules = [
+    'menu_ui',
     'node',
+    'user',
     'twig_render_this',
     'twig_render_this_test',
+    'views',
   ];
 
   /**
@@ -26,7 +29,7 @@ class TwigRenderThisTest extends BrowserTestBase {
   public function setUp() {
     parent::setUp();
 
-    $types = ['page', 'article'];
+    $types = ['article', 'page', 'product'];
 
     foreach ($types as $type) {
       $type = $this->createContentType(['type' => $type]);
@@ -36,23 +39,59 @@ class TwigRenderThisTest extends BrowserTestBase {
       ];
       $this->createNode($node_values);
     }
+
+    $nodes = [
+      'Start Trek TNG' => 'Star Trek: The Next Generation (TNG) is an American science fiction television series created by Gene Roddenberry.',
+      'Start Trek DS9' => 'Star Trek: Deep Space Nine (DS9) is an American science fiction television series created by Rick Berman and Michael Piller.',
+      'Start Trek Voyager' => 'Star Trek: Voyager is an American science fiction television series created by Rick Berman, Michael Piller, and Jeri Taylor.',
+    ];
+
+    foreach ($nodes as $title => $description) {
+      $this->createNode([
+        'type' => 'news',
+        'title' => $title,
+        'field_description' => $description,
+      ]);
+    }
   }
 
   /**
-   * Tests with renderThis filter.
+   * Tests rendering entities.
    */
-  public function testWithFilter() {
+  public function testEntities() {
+    $this->drupalGet('news');
+    $this->assertText('Using Twig Render This to render entities');
+    $this->assertText('Start Trek TNG');
+    $this->assertText('Star Trek: The Next Generation (TNG) is an American science fiction television series created by Gene Roddenberry.');
+    $this->assertText('Start Trek DS9');
+    $this->assertNoText('Star Trek: Deep Space Nine (DS9) is an American science fiction television series created by Rick Berman and Michael Piller.');
+    $this->assertText('Start Trek Voyager');
+    $this->assertNoText('Star Trek: Voyager is an American science fiction television series created by Rick Berman, Michael Piller, and Jeri Taylor.');
+  }
+
+  /**
+   * Tests rendering a field with renderThis filter.
+   */
+  public function testFieldWithFilter() {
     $this->drupalGet('node/1');
     $this->assertText('Hello world!');
     $this->assertText('This is Twig Render This');
   }
 
   /**
-   * Tests without renderThis filter.
+   * Tests rendering a field without renderThis filter.
    */
-  public function testWithoutFilter() {
+  public function testFieldWithoutFilter() {
     $this->drupalGet('node/2');
     $this->assertText('Object of type Drupal\Core\Field\FieldItemList cannot be printed');
+  }
+
+  /**
+   * Tests unsupported content.
+   */
+  public function testUnsupportedContent() {
+    $this->drupalGet('node/3');
+    $this->assertText('Twig Render This: Unsupported content.');
   }
 
 }
