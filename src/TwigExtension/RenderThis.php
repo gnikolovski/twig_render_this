c3b3624e120496ba2c2c55c79499566a9f89e67a
Goran Nikolovski <nikolovski84@gmail.com>
2024-07-12 12:51:48 +0200

    Issue #3435147 by Project Update Bot, gnikolovski: Automated Drupal 11 compatibility fixes for twig_render_this

--- src/TwigExtension/RenderThis.php
+++ src/TwigExtension/RenderThis.php
@@ -5,6 +5,7 @@ namespace Drupal\twig_render_this\TwigExtension;
 use Drupal\Core\Entity\EntityInterface;
 use Drupal\Core\Field\FieldItemInterface;
 use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\Core\StringTranslation\TranslatableMarkup;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFilter;
 
@@ -16,31 +17,31 @@ class RenderThis extends AbstractExtension {
   /**
    * {@inheritdoc}
    */
-  public function getName() {
+  public function getName(): string {
     return 'twig_render_this.twig_extension';
   }
 
   /**
    * {@inheritdoc}
    */
-  public function getFilters() {
+  public function getFilters(): array {
     return [
-      new TwigFilter('renderThis', [$this, 'renderThisFilter']),
+      new TwigFilter('renderThis', $this->renderThisFilter(...)),
     ];
   }
 
   /**
    * Returns the rendered array for a single entity field.
    *
-   * @param object $content
+   * @param mixed $content
    *   Entity or Field object.
    * @param string $view_mode
    *   Name of the display mode.
    *
-   * @return null|array
+   * @return null|array|TranslatableMarkup
    *   A rendered array for the field or NULL if the value does not exist.
    */
-  public static function renderThisFilter($content, $view_mode = 'default') {
+  public static function renderThisFilter(mixed $content, string $view_mode = 'default'): array|TranslatableMarkup|null {
     if ($content instanceof EntityInterface) {
       $view_builder = \Drupal::entityTypeManager()
         ->getViewBuilder($content->getEntityTypeId());
